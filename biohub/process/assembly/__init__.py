61ddf2238f97c5bad278e5476ae38493550f6ff9
JuanAntonioMS <b32masaj@gmail.com>
2023-02-09 14:59:05 +0100

    fix(Process Assembly): :fire: Deleted not included imports

--- biohub/process/assembly/__init__.py
+++ biohub/process/assembly/__init__.py
@@ -2,6 +2,3 @@ class Assembly:
     pass
 
 from biohub.process.assembly.unicycler import Unicycler
-from biohub.process.assembly.flye_assembler import FlyeAssembler
-from biohub.process.assembly.miniasm import Miniasm
-from biohub.process.assembly.raven import Raven
