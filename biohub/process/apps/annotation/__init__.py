288a72f9040f85557b6eebb746c0237e34265c20
JuanAntonioMS <b32masaj@gmail.com>
2023-05-05 16:27:33 +0200

    feat: :sparkles: NCBI AMRFINDER build up

--- biohub/process/apps/annotation/__init__.py
+++ biohub/process/apps/annotation/__init__.py
@@ -5,3 +5,4 @@ from biohub.process.apps.annotation.mlst import Mlst
 from biohub.process.apps.annotation.resfinder import ResFinder
 from biohub.process.apps.annotation.plasmidfinder import PlasmidFinder
 from biohub.process.apps.annotation.prokka import Prokka
+from biohub.process.apps.annotation.amrfinder import AMRFinder
